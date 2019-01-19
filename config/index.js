5f54bee372acff93e7e1c19a75e0d23825b21818
James Katarikawe <james.katarikawe@andela.com>
2019-01-19 14:01:45 +0300

    Deploy to GitHub Pages

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://jaymeskat.github.io/Vue-BookListing/',
 
     /**
      * Source Maps
